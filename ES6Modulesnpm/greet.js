e71dddd4e8b592088fed1af115dcbe47660ec323
Japsana Mulmi <japsana.mulmi@infodev.com.np>
2025-11-27 16:48:56 +0545

    Solved q2: Implemented Default Export

---
+++ ES6Modulesnpm/greet.js
@@ -0,0 +1,8 @@
+// Create greet.js exporting a default function that returns "Hello &lt;name&gt;" . Import it
+// as g in main.js .
+
+
+
+export default function(name) {
+    return `Hello ${name}`;
+}
